4deecb554426bbd43833ad951396c3b10580b1fd
Usama <hafizmusamabhutta2007@gmail.com>
2026-04-09 18:11:58 +0500

    Some progress, constructor, destructor,...

--- OopAssignment3_question1/Part2.h
+++ OopAssignment3_question1/Part2.h
@@ -1,2 +1,55 @@
 #pragma once
+class Organism;
 
+struct Tile
+{
+	float nutrientLevel;
+	float toxicity;
+	Organism* occupant;
+};
+
+class World
+{
+	Tile** tiles;
+	Organism** orgs;
+	int width, height;
+	int orgCount;
+	int orgCap;
+public:
+
+	World(int h, int w)
+	{
+		this->height = h, this->width = w;
+		tiles = new Tile * [h];
+		for (int i = 0; i < h; i++)
+		{
+			tiles[i] = new Tile [w];
+		}
+		for (int i = 0; i < h; i++)
+		{
+			for (int j = 0; j < w; j++)
+			{
+				tiles[i][j].nutrientLevel = (float)((rand() % 100001)) / 1000.0f;
+				tiles[i][j].toxicity = (float)((rand() % 100001)) / 1000.0f;
+			}
+		}
+		orgCap = 10;
+		orgCount = 0;
+		orgs = new Organism * [orgCap];
+	}
+	void runIteration()
+	{
+
+	}
+	
+	~World()
+	{
+		for (int i = 0; i < height; i++)
+			delete[] tiles[i];
+		delete[] tiles;
+
+		for (int i = 0; i < orgCount; i++)
+			delete orgs[i];
+		delete[] orgs;
+	}
+};
