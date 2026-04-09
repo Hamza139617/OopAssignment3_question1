eb4b6c05d497918d651e6374c7ef230284da9a18
Hamza <hamza139617@gmail.com>
2026-04-09 09:05:35 -0700

    working on the organisms

--- OopAssignment3_question1/part3.h
+++ OopAssignment3_question1/part3.h
@@ -8,18 +8,99 @@ class Organism {
 
 protected:
 	int x, y, width, height;
-	bool** internalShape;
+	bool** internalShape = nullptr ;
+	bool specie = false; // type of the organism initially false meaning fractalSprawler and true meaning kineticHunter 
+	int maxSize = 5;
+	
+	float getAverageNutrients(Tile** world) {
+
+	}
+
+public:
+	Organism(int x, int y, int width, int height) : x(x), y(y), width(width), height(height) {
+
+		
+
+		internalShape = new bool* [maxSize];	// when the organism is created the internalShape initially should be 2x2
+
+		for (int i = 0; i < maxSize; i++) {
+			internalShape[i] = new bool[maxSize];
+		}
+
+		for (int i = 0; i < width; i++) {
+
+			for (int j = 0; j < height ; j++) {
+				internalShape[i][j] = true;
+			}
+
+		}
+
+		
+
+	}
+
+	Organism() : x(0), y(0), width(0), height(0) {
+
+		
+
+		internalShape = new bool* [maxSize];	
+
+		for (int i = 0; i < maxSize; i++) {
+			internalShape[i] = new bool[maxSize];
+		}
+
+		for (int i = 0; i < width; i++) {
+
+			for (int j = 0; j < height; j++) {
+				internalShape[i][j] = true;
+			}
+
+		}
+
+
+
+	}
 
 	virtual void update(Tile** word, int worldWidth, int worldHeight);
 
-	float getAverageNutrients(Tile** world);
+	~Organism() {
+
+		for (int i = 0; i < maxSize; i++) {
+			delete[] internalShape[i];
+		}
+
+		delete[] internalShape;
+
+	}
 
 };
 
-class FractalSprawler {
+class FractalSprawler: public Organism {
+
+public:
+	FractalSprawler(int x, int y, int width, int height) : Organism(x, y, width, height) {
+		this->specie = false;
+	}
+
+	FractalSprawler() {
+		this->specie = false;
+	}
+
+	~FractalSprawler() {
+
+	}
 	
 };
 
-class KineticHunter {
+class KineticHunter: public Organism {
+
+public:
+	KineticHunter(int x, int y, int width, int height) : Organism(x, y, width, height) {
+		this->specie = true;
+	}
+
+	KineticHunter() {
+		this->specie = true;
+	}
 
 };
