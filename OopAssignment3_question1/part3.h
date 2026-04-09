f8fc7e5da19cc10c78a13e06c709f26d0f64eb54
Hamza <hamza139617@gmail.com>
2026-04-09 04:48:05 -0700

    brought the changes in the part3.h file added the relevant classes only

--- OopAssignment3_question1/part3.h
+++ OopAssignment3_question1/part3.h
@@ -1,1 +1,25 @@
 #pragma once
+
+
+class Tile;
+
+
+class Organism {
+
+protected:
+	int x, y, width, height;
+	bool** internalShape;
+
+	virtual void update(Tile** word, int worldWidth, int worldHeight);
+
+	float getAverageNutrients(Tile** world);
+
+};
+
+class FractalSprawler {
+	
+};
+
+class KineticHunter {
+
+};
