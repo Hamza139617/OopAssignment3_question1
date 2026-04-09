ac28180dc8ea37ff622f0f395c518e5eb3f0df13
Usama <hafizmusamabhutta2007@gmail.com>
2026-04-09 21:09:18 +0500

    getters

--- OopAssignment3_question1/Part2.h
+++ OopAssignment3_question1/Part2.h
@@ -1,4 +1,5 @@
 #pragma once
+#include<ctime>
 class Organism;
 
 struct Tile
@@ -6,6 +7,9 @@ struct Tile
 	float nutrientLevel;
 	float toxicity;
 	Organism* occupant;
+
+	float getNutrientLevel() const { return nutrientLevel; }
+	float getToxicity()const { return toxicity; }
 };
 
 class World
@@ -16,9 +20,9 @@ class World
 	int orgCount;
 	int orgCap;
 public:
-
 	World(int h, int w)
 	{
+		srand(time(0));
 		this->height = h, this->width = w;
 		tiles = new Tile * [h];
 		for (int i = 0; i < h; i++)
@@ -31,17 +35,30 @@ public:
 			{
 				tiles[i][j].nutrientLevel = (float)((rand() % 100001)) / 1000.0f;
 				tiles[i][j].toxicity = (float)((rand() % 100001)) / 1000.0f;
+				tiles[i][j].occupant = nullptr;
 			}
 		}
 		orgCap = 10;
 		orgCount = 0;
 		orgs = new Organism * [orgCap];
 	}
+	void addNewOrg()
+	{
+		if (orgCap == orgCount)
+		{
+			orgCap *= 2;
+			Organism** temp = new Organism*[orgCap];
+			delete[] orgs;
+			orgs = temp;
+		}
+		
+		orgs[orgCount] = new Organism;
+		orgCount++;
+	}
 	void runIteration()
 	{
 
 	}
-	
 	~World()
 	{
 		for (int i = 0; i < height; i++)
